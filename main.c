22b16676f08805cd5efbcdf6a04e55cac37e4ca7
pedroboas <116254023+pedroboas@users.noreply.github.com>
2023-01-11 18:42:02 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,56 @@
+#include<string.h>
+#include<stdio.h>
+#include<inserirproduto.h>
+
+
+int main(void){
+    int opcao;
+
+    printf("MENU:\n\n\n");
+    printf("1- Inserir produto:");
+    printf("2- Consulta de informacao produto:");
+    printf("3- Comprar produto:");
+    printf("4- Atualizar preço:");
+    printf("5- Reabastecer maquina:");
+    printf("6- Stock:");
+    printf("7- estatisticas e informacoes:");
+
+    printf("\n\nInsira a sua opcao:\n");
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+
+        break;
+
+    case 2:
+    
+        break;
+    
+    case 3:
+    
+        break;
+
+    case 4:
+    
+        break;        
+
+    case 5:
+    
+        break;
+    
+    case 6:
+
+        break;
+    
+    case 7:
+        break;
+    
+    default:
+
+    printf("opcao invalida.");
+        break;
+    }
+
+}
